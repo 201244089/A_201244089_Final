2ffea1d0e68d9be43a44eaba52ab4c8b9b50b3c7
LBH <dlqudgml0503@naver.com>
2016-12-09 11:05:00 +0900

    변수 생성

--- app/src/main/java/sampleandroid/a_201244089_final/MainActivity.java
+++ app/src/main/java/sampleandroid/a_201244089_final/MainActivity.java
@@ -2,8 +2,29 @@ package sampleandroid.a_201244089_final;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    Switch S_start;
+    Chronometer chronometer;
+    EditText et_adult,et_youth,et_child;
+    RadioGroup dcgroup;
+    RadioButton r_normaldc, r_cashdc, r_memberdc,r_date,r_time;
+    Button b_Pdone, b_gotime, b_tdone, b_back;
+    TextView tv_total,tv_dc,tv_cost;
+    ImageView imageView;
+    TimePicker timePicker;
+    CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
